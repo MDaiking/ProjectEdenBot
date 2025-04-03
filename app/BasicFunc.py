c4c9c95ebc990e6faac7e33ca5f174248badabe2
MDaiking <mdaiking117@gmail.com>
2025-04-03 11:02:31 +0900

    閉じる関数内でawaitしていない問題を修正

--- app/BasicFunc.py
+++ app/BasicFunc.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 async def close(client, message):
     if message.author.guild_permissions.administrator:
-        upmarin_mention = Config.get_upmarin_user(client).mention
+        upmarin_mention = await Config.get_upmarin_user(client).mention
         await message.channel.send('Botを停止します。'
                                    f'再度起動したい場合は {upmarin_mention} を呼んでください')
         await client.close()
